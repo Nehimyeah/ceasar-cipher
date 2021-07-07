33dac0e828305297535a888c00ba45638017e446
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-07-07 19:31:47 +0300

    Improve solution in a cleaner way

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,26 +1,14 @@
 def ceasar_cipher(string, shift)
-    plain_letters = string.split('')
-    cipher_letters = plain_letters.map do |letter|
-        if letter =~ /[a-z]/
-            ascii_value = letter.ord
-            ascii_value -= 96
-            ascii_value += shift
-            ascii_value = ascii_value % 26
-            ascii_value += 96
-            ascii_value.chr
-            
-        elsif letter =~ /[A-z]/
-            ascii_value = letter.ord
-            ascii_value -= 64
-            ascii_value += shift
-            ascii_value = ascii_value % 26
-            ascii_value += 64
-            ascii_value.chr
+    result = ''
+    string.each_byte do |ascii_value|
+        ascii_value <= 90 ? base = 64 : base = 96
+        if ascii_value.between?(65, 90) || ascii_value.between?(97, 122)
+            result << ((((ascii_value - base) + shift) % 26) + base).chr
         else
-            letter
+            result << ascii_value.chr
         end
     end
-    cipher_letters.join('')
+    result
 end
 
 ceasar_cipher("What a string!", 5)
